8b80f2c3c763e362408ceb003c6a33e08f41cf39
sam2829 <>
2023-07-28 17:39:45 +0000

    Add questions for the quiz to the script.js file

---
+++ assets/js/script.js
@@ -0,0 +1,152 @@
+let questions = [
+    {
+        question: "When Michael Jordan returned to basketball in 1995, he released a statement to the media. what did the statement say?",
+        answers: {
+            a: "I'm back",
+            b: "Lets go win a championship",
+            c: "I'm here to win",
+            d: "I'm better at basketball than baseball"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Where did Jurgen Klopp start his managerial career?",
+        answers: {
+            a: "Bayer Leverkusen",
+            b: "Oktoberfest",
+            c: "Mainz",
+            d: "Borussia Dortmund"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which cricketer has taken the most test wickets?",
+        answers: {
+            a: "Shane Warne",
+            b: "Glen McGrath",
+            c: "Muthiah Muralitharan",
+            d: "Eddie the eagle"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Who did Muhammad Ali fight in rumble in the jungle?",
+        answers: {
+            a: "Jason Bourne",
+            b: "George Foreman",
+            c: "Larry Holmes",
+            d: "Joe Frazier"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "In which sport would you commonly use the terms: Chukka, Neckshot and Millionaires shot?",
+        answers: {
+            a: "Polo",
+            b: "Bowls",
+            c: "Volley Ball",
+            d: "Rugby"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Which formula 1 team did Lewis Hamilton compete for from 2007 to 2012?",
+        answers: {
+            a: "Mercedes",
+            b: "Ferrari",
+            c: "Vauxhall",
+            d: "McLaren"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "What sport did Princess Anne compete in at the 1976 summer Olympic games?",
+        answers: {
+            a: "Judo",
+            b: "Equestrian",
+            c: "Dressage",
+            d: "Gymnastics"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "How many Olympic gold medals did Michael Phelps win?",
+        answers: {
+            a: "23",
+            b: "25",
+            c: "21",
+            d: "1"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Which female tennis player has won the most grand slams?",
+        answers: {
+            a: "Venus Williams",
+            b: "Margaret Court",
+            c: "Serena Williams",
+            d: "Kim Kardashian"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Which golfers have won the most PGA Tour events with 82 wins each?",
+        answers: {
+            a: "Tiger Woods & Jack Nicklaus",
+            b: "Jack Nicklaus & Nick Faldo",
+            c: "Tiger Woods & Sam Snead",
+            d: "Phil & Grant Mitchell"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Where did Sir Alex Ferguson start out his managerial career?",
+        answers: {
+            a: "St Mirren",
+            b: "Aberdeen",
+            c: "Mardy FC",
+            d: "East Stirlingshire"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Who is the Premier Leagues all time top goal scorer?",
+        answers: {
+            a: "Dean Windass",
+            b: "Alan Shearer",
+            c: "Harry Kane",
+            d: "Wayne Rooney"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What was Usain Bolts 100 meters record breaking time?",
+        answers: {
+            a: "45 Seconds",
+            b: "10.05 Seconds",
+            c: "9.58 Seconds",
+            d: "9.89 Seconds"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What year did England win the rugby world cup?",
+        answers: {
+            a: "2003",
+            b: "2007",
+            c: "1966",
+            d: "2022"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Who is the leading all time try scorer for Wales in rugby?",
+        answers: {
+            a: "George North",
+            b: "Shane Williams",
+            c: "Gareth Thomas",
+            d: "Hugh Llewellyn"
+        },
+        correctAnswer: "b"
+    },
+]
