a7f7ef6948ddac85a7c858b2c3a09d85f8d0713a
sam2829 <>
2023-07-30 13:28:08 +0000

    Add code to display congratulations and your score upon finishing the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,137 +4,137 @@ let questions = [
     {
         question: "When Michael Jordan returned to basketball in 1995, he released a statement to the media. what did the statement say?",
         answers: [
-            {text: "I'm back", correct: true},
-            {text: "Lets go win a championship", correct: false},
-            {text: "I'm here to win", correct: false},
-            {text: "I'm better at basketball than baseball", correct: false}
-],
-},
-    
+            { text: "I'm back", correct: true },
+            { text: "Lets go win a championship", correct: false },
+            { text: "I'm here to win", correct: false },
+            { text: "I'm better at basketball than baseball", correct: false }
+        ],
+    },
+
     {
         question: "Where did Jurgen Klopp start his managerial career?",
         answers: [
-            {text: "Bayer Leverkusen", correct: false},
-            {text: "Oktoberfest", correct: false},
-            {text: "Mainz", correct: true},
-            {text: "Borussia Dortmund", correct: false}
-    ],
+            { text: "Bayer Leverkusen", correct: false },
+            { text: "Oktoberfest", correct: false },
+            { text: "Mainz", correct: true },
+            { text: "Borussia Dortmund", correct: false }
+        ],
     },
     {
         question: "Which cricketer has taken the most test wickets?",
         answers: [
-            {text: "Shane Warne", correct: false},
-            {text: "Glen McGrath", correct: false},
-            {text: "Muthiah Muralitharan", correct: true},
-            {text: "Eddie the eagle", correct: false}
+            { text: "Shane Warne", correct: false },
+            { text: "Glen McGrath", correct: false },
+            { text: "Muthiah Muralitharan", correct: true },
+            { text: "Eddie the eagle", correct: false }
         ],
     },
     {
         question: "Who did Muhammad Ali fight in rumble in the jungle?",
         answers: [
-            {text: "Jason Bourne", correct: false},
-            {text: "George Foreman", correct: true},
-            {text: "Larry Holmes", correct: false},
-            {text: "Joe Frazier", correct: false}
+            { text: "Jason Bourne", correct: false },
+            { text: "George Foreman", correct: true },
+            { text: "Larry Holmes", correct: false },
+            { text: "Joe Frazier", correct: false }
         ],
     },
     {
         question: "In which sport would you commonly use the terms: Chukka, Neckshot and Millionaires shot?",
         answers: [
-            {text: "Polo", correct: true},
-            {text: "Bowls", correct: false},
-            {text: "Volley Ball", correct: false},
-            {text: "Rugby", correct: false}
+            { text: "Polo", correct: true },
+            { text: "Bowls", correct: false },
+            { text: "Volley Ball", correct: false },
+            { text: "Rugby", correct: false }
         ],
     },
     {
         question: "Which formula 1 team did Lewis Hamilton compete for from 2007 to 2012?",
         answers: [
-            {text: "Mercedes", correct: false},
-            {text: "Ferrari", correct: false},
-            {text: "Vauxhall", correct: false},
-            {text: "McLaren", correct: true}
+            { text: "Mercedes", correct: false },
+            { text: "Ferrari", correct: false },
+            { text: "Vauxhall", correct: false },
+            { text: "McLaren", correct: true }
         ],
     },
     {
         question: "What sport did Princess Anne compete in at the 1976 summer Olympic games?",
         answers: [
-            {text: "Judo", correct: false},
-            {text: "Equestrian", correct: true},
-            {text: "Dressage", correct: false},
-            {text: "Gymnastics", correct: false}
+            { text: "Judo", correct: false },
+            { text: "Equestrian", correct: true },
+            { text: "Dressage", correct: false },
+            { text: "Gymnastics", correct: false }
         ],
     },
     {
         question: "How many Olympic gold medals did Michael Phelps win?",
         answers: [
-            {text: "23", correct: true},
-            {text: "25", correct: false},
-            {text: "21", correct: false},
-            {text: "1", correct: false},
+            { text: "23", correct: true },
+            { text: "25", correct: false },
+            { text: "21", correct: false },
+            { text: "1", correct: false },
         ],
     },
     {
         question: "Which female tennis player has won the most grand slams?",
         answers: [
-            {text: "Venus Williams", correct: false},
-            {text: "Margaret Court", correct: true},
-            {text: "Serena Williams", correct: false},
-            {text: "Kim Kardashian", correct: false}
+            { text: "Venus Williams", correct: false },
+            { text: "Margaret Court", correct: true },
+            { text: "Serena Williams", correct: false },
+            { text: "Kim Kardashian", correct: false }
         ],
     },
     {
         question: "Which golfers have won the most PGA Tour events with 82 wins each?",
         answers: [
-            {text: "Tiger Woods & Jack Nicklaus", correct: false},
-            {text: "Jack Nicklaus & Nick Faldo", correct: false},
-            {text: "Tiger Woods & Sam Snead", correct: true},
-            {text: "Phil & Grant Mitchell", correct: false}
+            { text: "Tiger Woods & Jack Nicklaus", correct: false },
+            { text: "Jack Nicklaus & Nick Faldo", correct: false },
+            { text: "Tiger Woods & Sam Snead", correct: true },
+            { text: "Phil & Grant Mitchell", correct: false }
         ],
     },
     {
         question: "Where did Sir Alex Ferguson start out his managerial career?",
         answers: [
-            {text: "St Mirren", correct: false},
-            {text: "Aberdeen", correct: false},
-            {text: "Mardy FC", correct: false},
-            {text: "East Stirlingshire", correct: true}
+            { text: "St Mirren", correct: false },
+            { text: "Aberdeen", correct: false },
+            { text: "Mardy FC", correct: false },
+            { text: "East Stirlingshire", correct: true }
         ],
     },
     {
         question: "Who is the Premier Leagues all time top goal scorer?",
         answers: [
-            {text: "Dean Windass", correct: false},
-            {text: "Alan Shearer", correct: true},
-            {text: "Harry Kane", correct: false},
-            {text: "Wayne Rooney", correct: false},
+            { text: "Dean Windass", correct: false },
+            { text: "Alan Shearer", correct: true },
+            { text: "Harry Kane", correct: false },
+            { text: "Wayne Rooney", correct: false },
         ],
     },
     {
         question: "What was Usain Bolts 100 meters record breaking time?",
         answers: [
-            {text: "45 Seconds", correct: false},
-            {text: "10.05 Seconds", correct: false},
-            {text: "9.58 Seconds", correct: true},
-            {text: "9.89 Seconds", correct: false}
+            { text: "45 Seconds", correct: false },
+            { text: "10.05 Seconds", correct: false },
+            { text: "9.58 Seconds", correct: true },
+            { text: "9.89 Seconds", correct: false }
         ],
     },
     {
         question: "What year did England win the rugby world cup?",
         answers: [
-            {text: "2003", correct: true},
-            {text: "2007", correct: false},
-            {text: "1966", correct: false},
-            {text: "2022", correct: false}
+            { text: "2003", correct: true },
+            { text: "2007", correct: false },
+            { text: "1966", correct: false },
+            { text: "2022", correct: false }
         ],
     },
     {
         question: "Who is the leading all time try scorer for Wales in rugby?",
         answers: [
-            {text: "George North", correct: false},
-            {text: "Shane Williams", correct: true},
-            {text: "Gareth Thomas", correct: false},
-            {text: "Hugh Llewellyn", correct: false}
+            { text: "George North", correct: false },
+            { text: "Shane Williams", correct: true },
+            { text: "Gareth Thomas", correct: false },
+            { text: "Hugh Llewellyn", correct: false }
         ],
     },
 ];
@@ -145,6 +145,8 @@ let answerArea = document.getElementById("answer-area");
 let nextButton = document.getElementById("next-button");
 let correctScore = parseInt(document.getElementById("score").innerText);
 let incorrectScore = parseInt(document.getElementById("incorrect").innerText);
+
+
 // Wait for DOM to finish laoding before starting the quiz
 // Set all scores to 0 and call to start the quiz
 
@@ -155,55 +157,58 @@ document.addEventListener("DOMContentLoaded", function () {
 // Start Quiz and select a random question
 
 function startQuiz() {
-    displayQuestions();
    // randomQuestion();
+    displayQuestions();
+
 }
 
 //randomise the list of questions
 
-//function  randomQuestion() {
-  //  let randomQuestions = questions.sort(() => Math.random() - .5);
-    //return questions[randomQuestions];
-    
+//function randomQuestion() {
+   // let randomQuestions = questions.sort(() => Math.random() - .5);
+   // return questions[randomQuestions];
+
 //}
 
 //Display questions function
 
 function displayQuestions() {
 
-   resetPage();
+    resetPage();
+
+
 
     let currentQuestion = randomQuestions[questionNumber];
-    
+
     questionNumber = questionNumber + 1;
 
     document.getElementById("question").textContent = questionNumber + ". " + currentQuestion.question;
 
 
     // display answers with the current question
-    
+
     currentQuestion.answers.forEach(answer => {
-       const button = document.createElement("button");
-       button.innerHTML = answer.text;
-       button.classList.add("answer-buttons");
-       //button.classList.add("answer-a");
-       //button.classList.add("answer-b");
-       //button.classList.add("answer-c");
-       //button.classList.add("answer-d");
-       button.classList.add("answer-area");
-      
-       answerArea.appendChild(button);
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("answer-buttons");
+        //button.classList.add("answer-a");
+        //button.classList.add("answer-b");
+        //button.classList.add("answer-c");
+        //button.classList.add("answer-d");
+        button.classList.add("answer-area");
 
-       if (answer.correct) {
-        button.dataset.correct = answer.correct;
-       }
-     
+        answerArea.appendChild(button);
 
-       // When an answer button is clicked it will call the check answer function
-       button.addEventListener("click", checkAnswer);
-    })
-   
-   
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+
+
+        // When an answer button is clicked it will call the check answer function
+        button.addEventListener("click", checkAnswer);
+    });
+
+
 }
 
 
@@ -211,11 +216,11 @@ function displayQuestions() {
 
 function resetPage() {
 
-   nextButton.disabled = true;
+    nextButton.disabled = true;
 
-   while (answerArea.firstChild){
-    answerArea.removeChild(answerArea.firstChild);
-   }
+    while (answerArea.firstChild) {
+        answerArea.removeChild(answerArea.firstChild);
+    }
 }
 
 
@@ -234,7 +239,7 @@ function checkAnswer(e) {
         incrementIncorrectScore();
     }
 
-    
+
 
     Array.from(answerArea.children).forEach(button => {
         if (button.dataset.correct === "true") {
@@ -242,30 +247,30 @@ function checkAnswer(e) {
         }
         button.disabled = true;
 
-        
+
     });
-    
+
     // call for next question
     nextButton.disabled = false;
     nextButton.addEventListener("click", nextQuestion);
-    
+
 }
 
 
 // function to increment the correct score
 
 function incrementScore() {
-    
+
     document.getElementById("score").innerText = ++correctScore;
-   
+
 }
 
 // function to increment the incorrect score
 
 function incrementIncorrectScore() {
-   
+
     document.getElementById("incorrect").innerText = ++incorrectScore;
-    
+
 }
 
 // function to display the next question
@@ -285,13 +290,16 @@ function displayFinalScore() {
     //Hide original question, answers and next button areas and display play again button
     alert(`You scored ${correctScore} / 15!`);
     document.getElementById("answer-area").style.display = "none";
-    document.getElementById("question-area").style.display = "none";
     document.getElementById("next-button").style.display = "none";
     document.getElementById("play-again").style.display = "revert";
-  
+
+    //Display final score
+    
+    document.getElementById("question").innerText = `Congratulations! You scored ${correctScore} / 15!`;
+
 
     //click play again button to start quiz again
-
+    
     document.getElementById("play-again").addEventListener("click", restartQuiz);
 
 }
